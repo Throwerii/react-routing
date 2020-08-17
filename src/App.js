373130b0f6fcccba733569b29039e1c30fdea63c
Joe Thrower <jmthrower2@hotmail.com>
2020-08-17 11:47:00 -0400

    Final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { Route,Switch } from "react-router-dom";
 import './App.css';
+import Clock from './component/assets/clock/Clock'
+import Contact from './component/assets/contact/Contact'
+import Welcome from './component/assets/welcome/Welcome'
+import Navigation from "./component/assets/navigation/Navigation";
+import Error from './component/assets/error/Error'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Switch>
+        <Route
+          exact
+          path="/welcome/:name"
+          render={(props) => <Welcome {...props} />}
+        />
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Error}></Route> 
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
